1d13ba11532746e8459aed9b37404cb1fad166ed
Ahamed Saheer <saheer.ahamed570@gmail.com>
2023-02-26 03:29:33 +0530

    Created calculator

---
+++ src/digitButton.js
@@ -0,0 +1,12 @@
+import { ACTIONS } from "./app"
+
+
+function DigitButton({ dispatch, digit }) {
+    return (
+        <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit }})}>
+            {digit}
+        </button>
+    )
+}
+
+export default DigitButton
